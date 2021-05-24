8893f1271a0d767c88fe12ffa436a4e6de7cd7d7
unknown <kim.ornberg@skola.botkyrka.se>
2021-05-24 14:22:09 +0200

    finish

--- src/main.js
+++ src/main.js
@@ -1,20 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
+    target: document.body,
 });
 
 export default app;
-
-function loginfunc()
-{
-
-	if (document.getElementById('Password').value = "qwerty")
-	{
-		return true
-	} 
-	else 
-	{	
-		return false
-	}
-}
